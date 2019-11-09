674de5f8a05d95035a3ce8c7331718d075ff2d63
Tore Aurstad <tore.aurstad@hemit.no>
2019-11-09 23:00:46 +0100

    First commit

---
+++ parser.js
@@ -0,0 +1,43 @@
+var parserArithmeticSimpleModule = (function() {
+  parseMultiplicationSeparatedExpression = function(expression) {
+    var numbersString = expression.split("*");
+    var numbers = numbersString.map(function(item) {
+      return parseInt(item);
+    });
+    var total = numbers.reduce(multiplyPart);
+    function multiplyPart(total, num) {
+      return total * num;
+    }
+    return total;
+  };
+
+  parseMinusSeparatedExpression = function(expression) {
+    var numbersString = expression.split("-");
+    var numbers = numbersString.map(function(item) {
+      return parseMultiplicationSeparatedExpression(item);
+    });
+    var total = numbers.reduce(subtractPart);
+    function subtractPart(total, num) {
+      return total - num;
+    }
+    return total;
+  };
+
+  parsePlusSeparatedExpression = function(expression) {
+    var numbersString = expression.split("+");
+    var numbers = numbersString.map(function(item) {
+      return parseMinusSeparatedExpression(item);
+    });
+
+    var total = numbers.reduce(addPart);
+    function addPart(total, num) {
+      return total + num;
+    }
+    console.log(total);
+    return total;
+  };
+
+  return {
+    parseArithmeticExpression: parsePlusSeparatedExpression
+  };
+})();
